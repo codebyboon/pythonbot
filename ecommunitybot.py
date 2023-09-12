6640b36d26c7c1445c859f8619d34447816e4677
codebyboon <ah_boon95@hotmail.com>
2023-09-12 17:17:45 +0800

    Updated the bot to run parallel and won't auto close the browser upon successful/failure. Updated environment variables for the bot Updated whatsapp msg

--- ecommunitybot.py
+++ ecommunitybot.py
@@ -1,3 +1,5 @@
+import sys
+
 from typing import Tuple
 from time import sleep
 from datetime import time, datetime, timezone, timedelta
@@ -6,30 +8,35 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from joblib import Parallel, delayed
 
-#import whatsapp msg python script
+from selenium.common.exceptions import NoSuchElementException
+
+# access secret keys for the project
+from dotenv import dotenv_values
+import os
+
+# import whatsapp msg python script
 import whatsappmsg
 
 # params
-booking_site_url = 'https://www.ecommunity.my/'
-begin_time = time(00,00)
-end_time = time(00,15)
+secrets = dotenv_values('.env')
+booking_site_url = secrets['URL']
+begin_time = time(00, 00)
+end_time = time(23, 15)
 max_try = 500
+booked_time = ''
 # reservation_time and reservation_name are given as arguments when python script runs
-#reservation_time = int(sys.argv[1])
-#reservation_name = sys.argv[2]
+# reservation_time = int(sys.argv[1])
+# reservation_name = sys.argv[2]
 
-options = Options()
-# comment out this line to see the process in chrome
-options.add_argument('--headless')
-driver = webdriver.Chrome()
-#driver = webdriver.Chrome(options=options)
-    #'/Users/my_name/dev/stranger_bot/driver/chromedriver', 
-    #options=options
+
+# driver = webdriver.Chrome(options=options)
+# '/Users/my_name/dev/stranger_bot/driver/chromedriver',
+# options=options
 
 myt = timezone(timedelta(hours=+8), 'MYT')
 
 
-def check_current_time(begin_time:time, end_time:time) -> Tuple[time, bool]:
+def check_current_time(begin_time: time, end_time: time) -> Tuple[time, bool]:
     '''
     Check current time is between 00:00 and 00:15. 
     Returns current time and if it is between begin and end time.
@@ -39,52 +46,63 @@ def check_current_time(begin_time:time, end_time:time) -> Tuple[time, bool]:
     return current_time, (begin_time <= current_time) and (current_time < end_time)
 
 
-#def make_a_reservation(reservation_time:int, reservation_name:str) -> bool:
-def make_a_reservation() -> bool:
+# def make_a_reservation(reservation_time:int, reservation_name:str) -> bool:
+def make_a_reservation(index) -> bool:
     '''
     Make a reservation for the given time and name at the booking site.
     Return the status if the reservation is made successfully or not.
     '''
     try:
         # Open google chrome with the website
+        options = Options()
+        # comment out this line to see the process in chrome
+        # options.add_argument('--headless')
+        options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options)
         driver.get(booking_site_url)
 
         # Click Login button at homepage
-        driver.find_element(By.ID, 'menu-item-2273').click()
+        # Commented out since the element has been changed.
+        # driver.find_element(By.ID, 'menu-item-2273').click()
 
         # Auto submit username & password then click Login button
-        driver.find_element(By.NAME, 'username').send_keys('ah_boon95@hotmail.com')
+        driver.find_element(By.NAME, 'username').send_keys(
+            secrets['USERNAME'])
 
-        driver.find_element(By.NAME, 'password').send_keys('Jun2504!')
+        driver.find_element(By.NAME, 'password').send_keys(secrets['PASSWORD'])
 
-        driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/input[1]').click()
+        driver.find_element(
+            By.XPATH, '//*[@id="loginForm"]/div/div[3]/input[1]').click()
 
         # Click eBooking button/image
-        driver.find_element(By.XPATH, '//*[@id="feature-icons"]/div/table/tbody/tr[1]/td[3]/a/div').click()
+        driver.find_element(
+            By.XPATH, '//*[@id="feature-icons"]/div/table/tbody/tr[1]/td[3]/a/div').click()
 
         # Click Badminton court
         driver.implicitly_wait(1)
-        driver.find_element(By.XPATH, '/html/body/div[2]/section[1]/div/section[2]/div/a[3]/div').click()
-            
+        driver.find_element(
+            By.XPATH, '/html/body/div[2]/section[1]/div/section[2]/div/a[3]/div').click()
+
         # initialize the params
         current_time, running_time = check_current_time(begin_time, end_time)
         try_num = 1
 
-
         # repreat booking a reservation every second
         while True:
             if not running_time:
-                print(f'Not Running the program. It is {current_time} and not between {begin_time} and {end_time}')
+                print(
+                    f'Not Running the program. It is {current_time} and not between {begin_time} and {end_time}')
 
-                if current_time >= time(23,59,59):
-                   sleep(0.001)
-                elif time(23,59,58) <= current_time < time(23,59,59):
-                   sleep(0.05)
+                if current_time >= time(23, 59, 59):
+                    sleep(0.001)
+                elif time(23, 59, 58) <= current_time < time(23, 59, 59):
+                    sleep(0.05)
                 else:
-                   sleep(1)
+                    sleep(1)
 
                 try_num += 1
-                current_time, running_time = check_current_time(begin_time, end_time)
+                current_time, running_time = check_current_time(
+                    begin_time, end_time)
                 continue
 
             today_date = datetime.today()
@@ -92,75 +110,93 @@ def make_a_reservation() -> bool:
 
             print(f'----- try : {try_num} -----')
             # Agree to the T&C when it's 12.00a.m.
-            driver.find_element(By.XPATH, '//*[@id="myModal"]/div/div/div[3]/button[1]').click()
+            driver.find_element(
+                By.XPATH, '//*[@id="myModal"]/div/div/div[3]/button[1]').click()
 
-            if today_date.month != end_date.month:   
+            if today_date.month != end_date.month:
                 print('--going to next month page--')
-                #Go to next page
+                # Go to next page
                 driver.find_element(By.CLASS_NAME, 'arrow-right').click()
                 # Find Badminton available timeslot
                 greybox = driver.find_elements(By.CLASS_NAME, 'date_cell')
-            
-            else:             
+
+            else:
                 print('--remain at same page--')
                 # Find Badminton available timeslot
                 greybox = driver.find_elements(By.CLASS_NAME, 'date_cell')
 
             greybox[-1].click()
 
-            #Get all badminton time slot and select latest available slot
+            # Get all badminton time slot and select latest available slot
             latest = driver.find_elements(By.NAME, 'bookingTime')
-            latest[-1].click()
+            if index == 0:
+                latest[-1].click()
+            elif index == 1:
+                latest[-3].click()
 
             # Click add booking button after selecting latest time
-            driver.find_element(By.XPATH, '//*[@id="main"]/div/div/form/table/tbody/tr[15]/td/input').click()
-            
-            #Click Reserve button
-            driver.find_element(By.XPATH, '//*[@id="add-booking"]/div/form[1]/div[6]/button').click()
+            # driver.find_element(
+            #     By.XPATH, '//*[@id="main"]/div/div/form/table/tbody/tr[15]/td/input').click()
 
-            #Click Confirm button
-            driver.find_element(By.XPATH, '//*[@id="add-booking"]/div[1]/div[11]/button[2]').click()
+            driver.find_element(
+                By.XPATH, '//*[@id="main"]/div/div/form/table/tbody/tr[15]/td/input').click()
+
+            try:
+                # Click Reserve button
+                driver.find_element(
+                    By.XPATH, '//*[@id="add-booking"]/div/form[1]/div[6]/button').click()
+            except NoSuchElementException:
+                print('Cannot find Reserve button')
+
+            booked_time = driver.find_element(
+                By.NAME, 'displayStartTime').get_attribute('value')
+            print('Reserving court at time ' + booked_time)
+            # Click Confirm button
+            driver.find_element(
+                By.XPATH, '//*[@id="add-booking"]/div[1]/div[11]/button[2]')  # .click()
             return True
     except Exception as e:
-        print(e)
+        # print(e)
         return False
-    finally:
-	# close the drivers
-        print("success")
-        #driver.quit()
 
 
-#def try_booking(reservation_time:int, reservation_name:str, max_try:int=1000) -> None:
-def try_booking(max_try:int=1000) -> None:
+# def try_booking(reservation_time:int, reservation_name:str, max_try:int=1000) -> None:
+def try_booking(max_try: int = 1000) -> None:
     '''
     Try booking a reservation until either one reservation is made successfully or the attempt time reaches the max_try
     '''
     # initialize the params
     reservation_completed = False
 
-    # try to get ticket
-    #reservation_completed = make_a_reservation(reservation_time, reservation_name)
-    reservation_completed = make_a_reservation()
+    num_instances = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 
-    # Use Parallel to run multiple reservation attempts in parallel
-    '''
-    n_jobs = 4  # Set the number of parallel instances you want to run
-    results = Parallel(n_jobs=n_jobs)(
-        delayed(make_a_reservation)() for _ in range(n_jobs)
-    '''
+    if num_instances == 1:
+        # try to get ticket
+        # reservation_completed = make_a_reservation(reservation_time, reservation_name)
+        reservation_completed = make_a_reservation(0)
+    else:
+        # Use Parallel to run multiple reservation attempts in parallel
+        n_jobs = num_instances  # Set the number of parallel instances you want to run
+        reservation_completed = Parallel(n_jobs=n_jobs)(
+            delayed(make_a_reservation)(index) for index in range(n_jobs))
 
-    if reservation_completed:
+    if any(reservation_completed):
 
-        print('Reserved a badminton court')
+        print('Reserved a badminton court successfully.')
 
-        #To auto send successful reservation msg to whatsapp group
-        bookeddate = datetime.now() + timedelta(days=7)
-        formattedDate = bookeddate.strftime('%Y-%m-%d')
-        bookedday = bookeddate.strftime('%A')
-        whatsappmsg.message(formattedDate, bookedday)
+        # To auto send successful reservation msg to whatsapp group
+        # TODO: Uncomment this for actual use.
+        # bookeddate = datetime.now() + timedelta(days=7)
+        # formattedDate = bookeddate.strftime('%Y-%m-%d')
+        # bookedday = bookeddate.strftime('%A')
+        # whatsappmsg.message(formattedDate, bookedday)
     else:
         print('Failed to book')
 
-if __name__ == '__main__':    
-    #try_booking(reservation_time, reservation_name, max_try)
-    try_booking(max_try)
+
+if __name__ == '__main__':
+    # try_booking(reservation_time, reservation_name, max_try)
+    try:
+        try_booking(max_try)
+    except KeyboardInterrupt:
+        print("Execution interrupted by user.")
--- whatsappmsg.py
+++ whatsappmsg.py
@@ -2,24 +2,29 @@ import pywhatkit
 import pyautogui
 from pynput.keyboard import Key, Controller
 
+# access secret keys for the project
+from dotenv import dotenv_values
+import os
+
+# params
+secrets = dotenv_values('.env')
 keyboard = Controller()
 
+
 def send_message(bookingdate, bookingday):
-#def send_message():
+    # def send_message():
     try:
-        msg = "Hi everyone, successfully booked badminton court on ", bookingday, "(", bookingdate,"). Let us know if you cannot make it."
+        msg = "Hi everyone, successfully booked badminton court on ", bookingday, "(", bookingdate, "). Let us know if you cannot make it."
 
         pywhatkit.sendwhatmsg_to_group_instantly(
-            group_id="LSob8QLbZpd3L6OLtmk6cV",
+            group_id=secrets['WHATSAPP_1'],
             message=msg,
         )
-        #time.sleep(10)
+        # time.sleep(10)
         pyautogui.click()
-        #time.sleep(2)
+        # time.sleep(2)
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
         print("Message sent!")
     except Exception as e:
         print(str(e))
-
-
